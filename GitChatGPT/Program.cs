9528e1c256a35f5f2a3bfb19afd93e88e7abbdd8
Ali Benchaaban <ali.ben-chaaban@nexeo.group>
2023-05-07 13:08:53 +0200

    Add project files.

---
+++ GitChatGPT/Program.cs
@@ -0,0 +1,115 @@
+﻿using OpenAI.GPT3;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace GitChatGPT
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+			   .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddEnvironmentVariables()
+				.Build();
+
+			var apiKey = config.GetSection("apiKey").Get<string>();
+
+			while (true)
+			{
+				Console.Write(">");
+				string userInput = Console.ReadLine()?.ToLowerInvariant() ?? "";
+				string response = string.Empty;
+
+				var _openAIService = new OpenAIService(new OpenAiOptions()
+				{
+					ApiKey = apiKey
+				});
+
+				var _completionResult = await _openAIService.Completions.CreateCompletion(new CompletionCreateRequest()
+				{
+					Prompt = $"[SYSTEM]: Outpupt a git command only from the following instructions, no explanation or other text \n --- \n Instructions: {userInput} \n Git Command:",
+					Model = Models.TextDavinciV2,
+					Temperature = 0.5F,
+					MaxTokens = 100,
+					N = 1
+				});
+
+				if (_completionResult.Successful)
+				{
+					response = _completionResult.Choices[0].Text;
+				}
+				else
+				{
+					if (_completionResult.Error == null)
+					{
+						throw new Exception("Unknown Error");
+					}
+					Console.WriteLine($"{_completionResult.Error.Code}: {_completionResult.Error.Message}");
+				}
+
+				var gitCommands = response.Trim(' ', '\r', '\n')
+										  .Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (gitCommands == null)
+				{
+					Console.WriteLine(response);
+					continue;
+				}
+				foreach (var cmd in gitCommands)
+				{
+					string gitCommand = cmd.Trim().ToLowerInvariant();
+					if (gitCommand.StartsWith("git "))
+					{
+						Console.Write($"{gitCommand} (y/n)");
+						var confirm = Console.ReadLine()?.ToLower();
+						if ((confirm == "y") || (confirm == ""))
+						{
+							ExecuteCommand(gitCommand);
+						}
+						Console.WriteLine("");
+					}
+					else
+					{
+						Console.Write($"{gitCommand}");
+					}
+				}
+			}
+		}
+
+
+
+		static void ExecuteCommand(string command)
+		{
+			Process process = new Process();
+
+			process.StartInfo.FileName = "cmd.exe";
+			process.StartInfo.Arguments = "/c " + command;
+			process.StartInfo.UseShellExecute = false;
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.RedirectStandardError = true;
+			process.StartInfo.CreateNoWindow = true;
+
+			var ret = process.Start();
+			if (ret)
+			{
+				process.WaitForExit();
+				var output = process.StandardOutput.ReadToEnd();
+				if (!string.IsNullOrEmpty(output))
+					Console.WriteLine(output);
+				var error = process.StandardError.ReadToEnd();
+				if (!string.IsNullOrEmpty(error))
+					Console.WriteLine(error);
+			}
+			else
+			{
+				Console.WriteLine("failed to run..");
+			}
+		}
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Simplify Your Git Workflow: Introducing Git-ChatGPT, a Command Line Tool Empowered by ChatGPT 
+
+GitChatGPT is designed to utilize the ChatGPT API for interpreting user inputs and translating them into Git command lines. Upon generating the command line, GitChatGPT will execute it to carry out the corresponding Git action.
+
+The primary objective of developing GitChatGPT is to simplify the Git workflow and render it more user-friendly and approachable for beginners or those lacking familiarity with Git command lines.
+GitChatGPT facilitates this by enabling users to interact with Git via natural language input, which is more intuitive and easier to comprehend.
+
+You can learn about the solution from my articles on : [medium](https://medium.com/@alibenchaabene/simplify-your-git-workflow-introducing-git-chatgpt-a-command-line-tool-empowered-by-chatgpt-3f78a5fc1498)
+
+## Contribution
+This is an open-source project and contributors are what make such project with rich feature to learn, inspire, and motivate. Any contributions you make are greatly appreciated.
+
+## Fork the Project
+* Create your Branch (git checkout -b feature/FeatureName)
+* Commit your Changes (git commit -m 'Add some FeatureName')
+* Push to the Branch (git push origin feature/FeatureName)
+* Open a Pull Request
+
+## License
+Distributed under the MIT License.
+
+## Support
+This project is created for learning and sharing purpose. Additionally, this project can be used in your project. Please share your feedback or issues. Please support with star and share in your network
